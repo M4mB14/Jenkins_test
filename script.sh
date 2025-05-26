554bb49eaeb6474ebad7e089853ffccd3e2de94b
Luzgin_Sergey <luzgin05@inbox.ru>
2025-05-26 14:11:06 +0500

    new ex

--- script.sh
+++ script.sh
@@ -1,10 +1,5 @@
-#!/bin/bash
-
-# Запуск контейнера alpine в фоне
 docker run -d --name my_alpine_test --rm alpine sleep 60
 
-# Пауза на секунду, чтобы контейнер успел стартовать
 sleep 1
 
-# Вывод всех запущенных контейнеров
 docker ps
