030375153dfc388dd569660df56c08679f65d455
Luzgin_Sergey <luzgin05@inbox.ru>
2025-05-26 13:15:03 +0500

    Jenkins pipeline file

---
+++ Jenkinsfile.groovy
@@ -0,0 +1,33 @@
+pipeline {
+    agent any
+
+    stages {
+        stage('Клонирование репозитория') {
+            steps {
+                git 'https://github.com/M4mB14/Jenkins_test'
+            }
+        }
+
+        stage('Выполнение на удалённой машине') {
+            steps {
+                sshPublisher(
+                    publishers: [
+                        sshPublisherDesc(
+                            configName: 'Jenkins-agent-1', // Имя SSH-конфига, как ты настраивал ранее
+                            transfers: [
+                                sshTransfer(
+                                    sourceFiles: 'script.sh',
+                                    removePrefix: '',
+                                    remoteDirectory: '/home/ubuntu/jenkins-deploy',
+                                    execCommand: 'chmod +x script.sh && ./script.sh'
+                                )
+                            ],
+                            usePromotionTimestamp: false,
+                            verbose: true
+                        )
+                    ]
+                )
+            }
+        }
+    }
+}
