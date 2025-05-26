48d2368364328fce111df816901cd8fa652ab397
Luzgin_Sergey <luzgin05@inbox.ru>
2025-05-26 14:09:33 +0500

    new ex

--- script.sh
+++ script.sh
@@ -1,1 +1,10 @@
-echo "Hello from GitHub!" > jenkins_success
+#!/bin/bash
+
+# Запуск контейнера alpine в фоне
+docker run -d --name my_alpine_test --rm alpine sleep 60
+
+# Пауза на секунду, чтобы контейнер успел стартовать
+sleep 1
+
+# Вывод всех запущенных контейнеров
+docker ps
